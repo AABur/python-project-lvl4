e6634464c8d24ae81ef1fd26d7631e28b6354d0d
AABur <aabur@mail.ru>
2021-09-01 09:22:52 +0300

    update

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import django_heroku
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+dotenv_file = Path(BASE_DIR, '.env')
+if os.path.isfile(dotenv_file):
+    load_dotenv(dotenv_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xrukya5(8+zi$$7@zsy%d)+^s*qbyhd$(77k$+#g@_r04^ri4d'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
