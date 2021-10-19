b7e6e6ce1abcf09355ad5532241de9691779e10b
AABur <aabur@mail.ru>
2021-10-19 21:32:05 +0300

    feat: :sparkles: add edit-delete users field

--- users/tables.py
+++ users/tables.py
@@ -2,14 +2,18 @@ import django_tables2 as tables
 from django_tables2.utils import A
 
 from users.models import TMUser
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 
 class UserTable(tables.Table):
-    edit = tables.Column(
-        linkify=(
-            'user-update',
-            {'pk': A('pk')}
-        ),
+    TEMPLATE = '''
+        <a href="{% url 'user-update' record.pk %}" class="tbl_icon edit">Edit</a>
+        <a href="{% url 'user-delete' record.pk %}" class="tbl_icon delete">Delete</a>
+    '''
+
+    control = tables.TemplateColumn(
+        TEMPLATE,
         empty_values=(),
         verbose_name=''
     )
@@ -18,6 +22,3 @@ class UserTable(tables.Table):
         model = TMUser
         template_name = "django_tables2/bootstrap.html"
         fields = ('id', "username", 'full_name', 'date_joined')
-
-    def render_edit(self):
-        return 'Edit'
