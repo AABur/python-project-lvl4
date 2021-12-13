562c7818ff07aa5d0144d2560cf75b96ab7d1ba1
AABur <aabur@mail.ru>
2021-12-14 01:16:16 +0200

    test: :white_check_mark: add test user creation

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+import pytest
+from .models import TMUser
+
+
+@pytest.mark.django_db
+def test_user_create():
+    user = TMUser.objects.create(
+        first_name="John",
+        last_name="Doe",
+        username="john@gmail.com",
+    )
+    assert user.full_name() == "John Doe"
