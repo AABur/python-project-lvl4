7871e3018513341e15066f4bb93c020ec37c5b76
AABur <aabur@mail.ru>
2022-02-22 23:40:08 +0100

    feat: :triangular_flag_on_post: add status creation

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,21 +1,25 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django_tables2 import SingleTableView
 
+from task_manager.statuses.forms import StatusForm
 from task_manager.statuses.models import Status
 from task_manager.statuses.tables import StatusesListTable
 
-# Create your views here.
-
 
 class StatusesListView(SingleTableView):
-    template_name = 'statuses/statuses_list.html'
     model = Status
     table_class = StatusesListTable
-
-
-class StatusCreateView(SingleTableView):
     template_name = 'statuses/statuses_list.html'
+
+
+class StatusCreateView(SuccessMessageMixin, CreateView):
     model = Status
-    table_class = StatusesListTable
+    form_class = StatusForm
+    template_name = 'statuses/create.html'
+    success_url = reverse_lazy('statuses')
+    success_message = "Status successfully created"
 
 
 class StatusUpdateView(SingleTableView):
