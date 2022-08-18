36a14d5937da71d314c6e8e971d7ef18d99609b9
Alexander Burchenko <aabur@mail.ru>
2022-08-18 15:20:25 +0200

    fix: :globe_with_meridians: fix translations

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,19 +1,20 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-    success_message = "Home"
+    success_message = _('Home')
 
 
 class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
-    success_message = "Success login"
+    success_message = _('Success login')
 
 
-class UserLogoutView(SuccessMessageMixin, LoginView):
-    success_message = "Success logout"
-    success_url = reverse_lazy('users')
+class UserLogoutView(SuccessMessageMixin, LogoutView):
+    success_message = _('Success logout')  # FIXME
+    # success_url = reverse_lazy('users')
