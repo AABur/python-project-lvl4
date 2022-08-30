1da231a9a9f45216abba2dad8a834c0408debedf
Alexander Burchenko <aabur@mail.ru>
2022-08-30 23:12:35 +0300

    docs: :memo: add docstrings

--- users/models.py
+++ users/models.py
@@ -1,16 +1,19 @@
+"""Represents TMUser objects in the database."""
 from django.contrib.auth.models import AbstractUser
 from django.urls.base import reverse
 
 
 class TMUser(AbstractUser):
+    """Represents TMUser objects in the database."""
 
     def full_name(self):
+        """Return the full name of the user."""
         return self.get_full_name()
 
     def get_absolute_url(self):
-        """Returns the url to access a particular author instance."""
+        """Return the url to access a particular author instance."""
         return reverse('user-detail', args=[str(self.id)])
 
     def __str__(self):
-        """String for representing the Model object."""
+        """Represent the TMUser object."""
         return '{0}, {1}'.format(self.last_name, self.first_name)
