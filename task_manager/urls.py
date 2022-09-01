20cc301915f2fc2241570a90e340334f0f02644c
Alexander Burchenko <aabur@mail.ru>
2022-09-01 22:29:49 +0300

    fix: :bug: fix urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -46,6 +46,7 @@ urlpatterns += [
     path('users/', UsersListView.as_view(), name='users'),
     path('users/create/', SignUpView.as_view(), name='signup'),
     path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
+    path('user/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
 ]
 
 urlpatterns += [
