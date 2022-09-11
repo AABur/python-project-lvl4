7b02ebfff83d0f34f1872b09fd32e918efb9969b
Alexander Burchenko <aabur@mail.ru>
2022-09-11 22:12:11 +0200

    chore: :recycle: refactor model inner ordering

--- users/models.py
+++ users/models.py
@@ -6,14 +6,14 @@ from django.urls.base import reverse
 class TMUser(AbstractUser):
     """Represents TMUser objects in the database."""
 
-    def full_name(self):
-        """Return the full name of the user."""
-        return self.get_full_name()
+    def __str__(self):
+        """Represent the TMUser object."""
+        return '{0}, {1}'.format(self.last_name, self.first_name)
 
     def get_absolute_url(self):
         """Return the url to access a particular author instance."""
         return reverse('user-detail', args=[str(self.id)])
 
-    def __str__(self):
-        """Represent the TMUser object."""
-        return '{0}, {1}'.format(self.last_name, self.first_name)
+    def full_name(self):
+        """Return the full name of the user."""
+        return self.get_full_name()
