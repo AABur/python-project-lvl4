4c8287fc418bef3704e3dcfb40be79152691cb8b
Alexander Burchenko <aabur@mail.ru>
2022-09-15 02:56:56 +0300

    test: :white_check_mark: add tasks tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 
 from users.models import TMUser
+from statuses.models import Status
+from labels.models import Label
+from tasks.models import Task
 
 
 @pytest.fixture(autouse=True)
@@ -27,3 +30,62 @@ def user_other():
         username='guidovan',
         password='topsecret456',
     )
+
+
+@pytest.fixture
+def status_used():
+    return Status.objects.create(name='Used')
+
+
+@pytest.fixture
+def status_unused():
+    return Status.objects.create(name='Unused')
+
+
+@pytest.fixture
+def label_used():
+    return Label.objects.create(name='Used')
+
+
+@pytest.fixture
+def label_unused():
+    return Label.objects.create(name='Unused')
+
+
+@pytest.fixture
+def user_author():
+    return TMUser.objects.create(
+        first_name='Author',
+        last_name='Author',
+        username='author',
+        password='topsecret789',
+    )
+
+
+@pytest.fixture
+def user_executor():
+    return TMUser.objects.create(
+        first_name='Executor',
+        last_name='Executor',
+        username='executor',
+        password='topsecret101',
+    )
+
+
+@pytest.fixture
+def task(user_author, user_executor, status_used, label_used):
+    return Task.objects.create(
+        title='Task title',
+        description='Task description',
+        author=user_author,
+        executor=user_executor,
+        status=status_used,
+        label=label_used,
+    )
+
+
+@pytest.fixture
+def logged_in_client(client, user):
+    """Log in the client with the given user."""
+    client.force_login(user)
+    return client
---
+++ tests/test_tasks.py
@@ -0,0 +1,31 @@
+from django.urls import reverse
+from tasks.models import Task
+from http import HTTPStatus
+
+
+def test_create_task(client, user_author, user_executor, status_used, label_used):
+    assert Task.objects.count() == 0
+
+    client.force_login(user_author)
+    response = client.get(reverse('task-create'))
+    assert response.status_code == HTTPStatus.OK
+
+    response = client.post(reverse('task-create'), data={
+        'name': 'Task title',
+        'description': 'Task description',
+        'author': user_author.id,
+        'executor': user_executor.id,
+        'status': status_used.id,
+        'label': label_used.id,
+    })
+    assert Task.objects.count() == 1
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.url == reverse('tasks')
+
+    task = Task.objects.get(name='Task title')
+    assert task.name == 'Task title'
+    assert task.description == 'Task description'
+    assert task.author == user_author
+    assert task.executor == user_executor
+    assert task.status == status_used
+    assert task.label == label_used
