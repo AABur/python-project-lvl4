7b98c501c0b0c5a7d9fd26819fbcf7b145fd0ae0
Alexander Burchenko <aabur@mail.ru>
2022-09-15 03:44:11 +0300

    refactor: :coffin: delete unused code

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -41,10 +41,3 @@ urlpatterns += [
     path('tasks/', include('tasks.urls')),
     path('users/', include('users.urls')),
 ]
-
-# urlpatterns += [
-#     path('users/', UsersListView.as_view(), name='users'),
-#     path('users/create/', SignUpView.as_view(), name='signup'),
-#     path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
-#     path('user/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
-# ]
