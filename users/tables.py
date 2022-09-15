8fda7129952fdfc87ff1f26a3c4cfa1335d77d93
Alexander Burchenko <aabur@mail.ru>
2022-09-16 00:07:18 +0300

    refactor: :recycle: refactor imports

--- users/tables.py
+++ users/tables.py
@@ -1,7 +1,7 @@
 import django_tables2 as tables
 from django.utils.translation import gettext_lazy as _
 
-from users.models import TMUser
+from .models import TMUser
 
 TEMPLATE = '''
     {% load i18n %}
