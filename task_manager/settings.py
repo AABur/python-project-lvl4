3ee8ae1ff55fbe7bfc497d67755aac13b8c89e59
Alexander Burchenko <aabur@mail.ru>
2022-12-10 14:15:44 +0100

    fix: :bug: fix Django SECRET_KEY

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ if dotenv_file.is_file():
     load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
